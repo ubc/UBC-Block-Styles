61c507d81df7b89a4d016c1b266378867582160a
Kelvin <kelvin@longevitygraphics.com>
2020-04-01 09:43:59 -0700

    add clf style to tabs block

--- blocks/ubc-tabs-block/js/block.js
+++ blocks/ubc-tabs-block/js/block.js
@@ -3,3 +3,8 @@ wp.blocks.registerBlockStyle( 'ubc/tabs', {
     name: 'gradient-green-tabs',
     label: 'Green Gradient'
 } );
+
+wp.blocks.registerBlockStyle( 'ubc/tabs', {
+    name: 'clf',
+    label: 'UBC Brand Style'
+} );
