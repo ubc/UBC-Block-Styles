12727bdd56e7bbed58e1f3f9106fc776b7e23aeb
Rich Tape <richard@iamfriendly.com>
2023-02-26 14:15:18 -0800

    Added full width tertiary divider

--- blocks/separator-block/js/block.js
+++ blocks/separator-block/js/block.js
@@ -3,3 +3,9 @@ wp.blocks.registerBlockStyle( 'core/separator', {
     name: 'pathways-wide-tertiary',
     label: 'Wide Tertiary'
 } );
+
+// Full Tertiary Style
+wp.blocks.registerBlockStyle( 'core/separator', {
+    name: 'pathways-full-tertiary',
+    label: 'Full Tertiary'
+} );
