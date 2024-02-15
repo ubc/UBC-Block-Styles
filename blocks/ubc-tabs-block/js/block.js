458dcc70cb32e4f80c23c98ba717daf50978dc05
Rich Tape <richard.tape@ubc.ca>
2024-02-14 18:08:15 -0800

    Added CTLT-style secondary blue bg style for tabs

--- blocks/ubc-tabs-block/js/block.js
+++ blocks/ubc-tabs-block/js/block.js
@@ -1,10 +1,15 @@
 // Green Gradient Style
-wp.blocks.registerBlockStyle( 'ubc/tabs', {
-    name: 'gradient-green-tabs',
-    label: 'Green Gradient'
-} );
+wp.blocks.registerBlockStyle('ubc/tabs', {
+	name: 'gradient-green-tabs',
+	label: 'Green Gradient'
+});
 
-wp.blocks.registerBlockStyle( 'ubc/tabs', {
-    name: 'clf',
-    label: 'UBC Brand Style'
-} );
+wp.blocks.registerBlockStyle('ubc/tabs', {
+	name: 'clf',
+	label: 'UBC Brand Style'
+});
+
+wp.blocks.registerBlockStyle('ubc/tabs', {
+	name: 'secondary-blue-bg',
+	label: 'UBC Secondary Blue BG'
+});
